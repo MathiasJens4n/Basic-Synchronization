78a5dfc62dd7985cecd1af83121fe67b406a1e29
Mathias Jensen <mathiasbeckj@gmail.com>
2022-05-02 10:51:57 +0200

    Add project files.

---
+++ Opgave 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Synkronosering_Øvelser
+{
+    internal class Program
+    {
+        static readonly object locker = new object();
+        static int count;
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Counter);
+            Thread t2 = new Thread(Counter);
+
+
+            t1.Start(2);
+            t2.Start(-1);
+
+            Console.ReadKey();
+        }
+        static void Counter(object num)
+        {
+            while (true)
+            {
+                Monitor.Enter(locker);
+
+                try
+                {
+                    count += Convert.ToInt32(num);
+
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine(count);
+
+                }
+                finally
+                {
+                    Monitor.Exit(locker);
+                }
+            }
+        }
+    }
+}
---
+++ Opgave 2-3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Opgave_2
+{
+    internal class Program
+    {
+        static readonly object locker = new object();
+        static int total;
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Print);
+            Thread t2 = new Thread(Print);
+
+            t1.Start("*");
+            t2.Start("#");
+        }
+        static void Print(object text)
+        {
+            while (true)
+            {
+                Monitor.Enter(locker);
+                try
+                {
+                    for (int i = 1; i <= 60; i++)
+                    {
+                        Console.Write(text.ToString());
+                        if (i == 60)
+                        {
+                            total += i;
+                        }
+                    }
+                    Console.Write($" {total}\n");
+
+                    Thread.Sleep(1000);
+                }
+                finally
+                {
+
+                    Monitor.Exit(locker);
+                }
+            }
+        }
+    }
+}
